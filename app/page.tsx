e7b4131af0216fd2a29a89b60d32888404c99132
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2024-07-25 21:06:20 +0800

    added reviews animation on hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -85,7 +86,7 @@ export default function Home() {
       </section>
 
       {/* value proposition section */}
-      <section className="bg-slate-100 py-24">
+      <section className="bg-slate-100 grainy-dark py-24">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our
@@ -95,15 +96,17 @@ export default function Home() {
             </h2>
             <img src="snake-2.png" className="w-24 order-0 lg:order-2" alt="" />
           </div>
+
           {/* first user review */}
           <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 text-green-600 fill-green-600" />
-                <Star className="h-5 text-green-600 fill-green-600" />
-                <Star className="h-5 text-green-600 fill-green-600" />
-                <Star className="h-5 text-green-600 fill-green-600" />
-                <Star className="h-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -112,19 +115,18 @@ export default function Home() {
               </div>
               <div className="flex gap-4 mt-2">
                 <img className="rounded-full h-12 w-12 object-cover" src="users/user-1.png" alt="User" />
-              </div>
-              <div className="flex flex-col">
-                <p className="font-semibold">Jousheen</p>
-                <div className="flex gap-1.5 items-center text-zinc-600">
-                  <Check className="h-4 w-4 stroke-[3px] text-green-600" />
-                  <p className="text-sm">Verified Purchase</p>
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jousheen</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* second user review */}
-          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+
+            {/* second user review */}
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
                 <Star className="h-5 text-green-600 fill-green-600" />
@@ -135,17 +137,21 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  {`"`}I usually keep my phone together with my keys in my pocket that led to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>. I dig it.{`"`}
+                  {`"`}I usually keep my phone together with my keys in my pocket that led to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it.{`"`}
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
                 <img className="rounded-full h-12 w-12 object-cover" src="users/user-4.jpg" alt="User" />
-              </div>
-              <div className="flex flex-col">
-                <p className="font-semibold">Reanne</p>
-                <div className="flex gap-1.5 items-center text-zinc-600">
-                  <Check className="h-4 w-4 stroke-[3px] text-green-600" />
-                  <p className="text-sm">Verified Purchase</p>
+                <div className="flex flex-col">
+                  <p className="font-semibold">Reanne</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -153,8 +159,10 @@ export default function Home() {
         </MaxWidthWrapper>
 
         {/* animated part of the hero page */}
-        <div></div>
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
-    </div>
+    </div >
   );
 }
