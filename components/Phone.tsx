0274fd7806aa3c5291e043f6c8d76d689f3a2610
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2024-07-25 14:55:33 +0800

    added env and the user testimonials

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -8,9 +8,16 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
 
 const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
 
-    return (<div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}
-        {...props}>
-    </div>)
+    return <>
+        <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}
+            {...props}>
+            <img src={dark ? "phone-template-dark-edges.png" : "phone-template-white-edges.png"} alt="Phone Image" className="pointer-events-none z-50 select-none" />
+
+            <div className="absolute -z-10 inset-0">
+                <img src={imgSrc} className="object-cover" alt="overlaying phone image" />
+            </div>
+        </div>
+    </>
 }
 
 export default Phone
